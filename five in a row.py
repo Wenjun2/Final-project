d55a319988879f90a77507f06b3422f983f3729f
WENJUN MA <wenjun2@illinois.edu>
2017-05-10 22:11:55 -0500

    after click , write the method we can check which side win, four situation and 2 results.

--- five in a row.py
+++ five in a row.py
@@ -40,4 +40,51 @@ def Click():
                     q[i][j].draw(win)
                     q[i][j].setFill('white')
                p[15][15]+=1
+def Check():
+    for i in range(15):
+        for j in range(11):
+            if black[i+1][j+1] and black[i+1][j+2] and black[i+1][j+3] and black[i+1][j+4] and black[i+1][j+5]:
+               return 1
+               break
+            if white[i+1][j+1] and white[i+1][j+2]and white[i+1][j+3]and white[i+1][j+4]and white[i+1][j+5]:
+               return 2
+               break
+    for i in range(11):
+        for j in range(15):
+            if black[i+1][j+1] and black[i+2][j+1]and black[i+3][j+1]and black[i+4][j+1]and black[i+5][j+1]:
+               return 1
+               break
+            if white[i+1][j+1] and white[i+2][j+1]and white[i+3][j+1]and white[i+4][j+1]and white[i+5][j+1]:
+               return 2
+               break
+    for i in range(11):
+        for j in range(11):
+            if black[i+1][j+1] and black[i+2][j+2]and black[i+3][j+3]and black[i+4][j+4]and black[i+5][j+5]:
+               return 1
+               break
+            if white[i+1][j+1] and white[i+2][j+2]and white[i+3][j+3]and white[i+4][j+4]and white[i+5][j+5]:
+               return 2
+               break
+    for i in range(11):
+        for j in range(15):
+            if black[i+1][j+1] and black[i+2][j]and black[i+3][j-1]and black[i+4][j-2]and black[i+5][j-3]:
+               return 1
+               break
+            if white[i+1][j+1] and white[i+2][j]and white[i+3][j-1]and white[i+4][j-2]and white[i+5][j-3]:
+               return 2
+               break
+
+def main():
+    WinBoard()
+    while 1:
+        Click()
+        Check()
+        if Check()==1:
+            Text(Point(240,550),'the black wins').draw(win)
+            break
+        if Check()==2:
+            Text(Point(240,550),'the white wins').draw(win)
+            break
+    win.getMouse()
+main()
 
